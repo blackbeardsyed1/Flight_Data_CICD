645de93c781d0e50664ef72566e0763597dea9b8
Syed <syed123khizar@gmail.com>
2025-02-04 06:33:15 +0500

    Checking Airflow

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -11,7 +11,7 @@ default_args = {
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2025, 2, 5),
+    'start_date': datetime(2025, 1, 5),
 }
 
 # Define the DAG
